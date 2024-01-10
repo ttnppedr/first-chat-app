72af07f85dc3e66222ebf0feefa1c4c9f5d894d1
ttnppedr <priest0204@gmail.com>
2024-01-10 09:30:35 +0800

    Add message service contract

---
+++ chat/lib/src/services/message/message_service_contract.dart
@@ -0,0 +1,8 @@
+import '../../models/message.dart';
+import '../../models/user.dart';
+
+abstract class IMessageService {
+  Future<bool> send(Message message);
+  Stream<Message> messages({required User activeUser});
+  dispose();
+}

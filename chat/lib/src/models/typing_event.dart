76ecee98831ca872d91d2bbaef96fa64dffbde9e
ttnppedr <priest0204@gmail.com>
2024-01-10 16:48:54 +0800

    Add Typingevent model

---
+++ chat/lib/src/models/typing_event.dart
@@ -0,0 +1,37 @@
+enum Typing { start, stop }
+
+extension TypingParsing on Typing {
+  String value() {
+    return this.toString().split('.').last;
+  }
+
+  static Typing fromString(String status) {
+    return Typing.values.firstWhere((element) => element.value() == status);
+  }
+}
+
+class TypingEvent {
+  String? get id => _id;
+  final String? from;
+  final String? to;
+  final Typing event;
+  String? _id;
+
+  TypingEvent({required this.from, required this.to, required this.event});
+
+  Map<String, dynamic> toJson() => {
+        'from': from,
+        'to': to,
+        'event': event.value(),
+      };
+
+  factory TypingEvent.fromJson(Map<String, dynamic> json) {
+    final event = TypingEvent(
+      from: json['from'],
+      to: json['to'],
+      event: TypingParsing.fromString(json['status']),
+    );
+    event._id = json['id'];
+    return event;
+  }
+}

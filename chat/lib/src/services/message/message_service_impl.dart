e2d46385a00c1cbf4c55fbf76d69bec47333d92b
ttnppedr <priest0204@gmail.com>
2024-01-10 10:39:57 +0800

    Encrypt message

--- chat/lib/src/services/message/message_service_impl.dart
+++ chat/lib/src/services/message/message_service_impl.dart
@@ -2,17 +2,19 @@ import 'dart:async';
 
 import 'package:chat/src/models/message.dart';
 import 'package:chat/src/models/user.dart';
+import 'package:chat/src/services/encryption/encryption_contract.dart';
 import 'package:chat/src/services/message/message_service_contract.dart';
 import 'package:rethink_db_ns/rethink_db_ns.dart';
 
 class MessageService implements IMessageService {
   final Connection? _connection;
   final RethinkDb r;
+  final IEncryption _encryption;
 
   final _controller = StreamController<Message>.broadcast();
   StreamSubscription? _changeFeed;
 
-  MessageService(this.r, this._connection);
+  MessageService(this.r, this._connection, this._encryption);
 
   @override
   dispose() {
@@ -28,8 +30,9 @@ class MessageService implements IMessageService {
 
   @override
   Future<bool> send(Message message) async {
-    Map record =
-        await r.table('messages').insert(message.toJson()).run(_connection!);
+    var data = message.toJson();
+    data['contents'] = _encryption.encrypt(data['contents']);
+    Map record = await r.table('messages').insert(data).run(_connection!);
     return record['inserted'] == 1;
   }
 
@@ -56,7 +59,9 @@ class MessageService implements IMessageService {
   }
 
   Message _messageFromFeed(feedData) {
-    return Message.fromJson(feedData['new_val']);
+    var data = feedData['new_val'];
+    data['contents'] = _encryption.decrypt(data['contents']);
+    return Message.fromJson(data);
   }
 
   _removeDeliveredMessage(Message message) {

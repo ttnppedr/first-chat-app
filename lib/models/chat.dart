e05679df16a7a4da01f2cf2e64f8fc791ccba4cc
ttnppedr <priest0204@gmail.com>
2024-01-13 18:21:52 +0800

    Add models

---
+++ lib/models/chat.dart
@@ -0,0 +1,13 @@
+import 'local_message.dart';
+
+class Chat {
+  String id;
+  int unread = 0;
+  List<LocalMessage>? messages = [];
+  LocalMessage? mostRecent;
+
+  Chat(this.id, {this.messages, this.mostRecent});
+
+  toMap() => {'id': id};
+  factory Chat.fromMap(Map<String, dynamic> json) => Chat(json['id']);
+}

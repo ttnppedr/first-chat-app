0523b63f176a9c9192c1c8bc616a9fd6ee5db18b
ttnppedr <priest0204@gmail.com>
2024-01-19 12:37:29 +0800

    Add onboarding ui

---
+++ lib/ui/pages/onboarding/onboarding.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import '../../widgets/logo.dart';
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _logo(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+_logo(BuildContext context) {
+  return Row(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Text('chat',
+          style: Theme.of(context)
+              .textTheme
+              .headlineMedium!
+              .copyWith(fontWeight: FontWeight.bold)),
+      SizedBox(width: 8),
+      Logo(),
+      SizedBox(width: 8),
+      Text('chat',
+          style: Theme.of(context)
+              .textTheme
+              .headlineMedium!
+              .copyWith(fontWeight: FontWeight.bold)),
+    ],
+  );
+}
---
+++ lib/ui/widgets/logo.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../../theme.dart';
+
+class Logo extends StatelessWidget {
+  const Logo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: isLightTheme(context)
+            ? Image.asset(
+                'assets/logo_light.png',
+                fit: BoxFit.fill,
+              )
+            : Image.asset(
+                'assets/logo_dark.png',
+                fit: BoxFit.fill,
+              ));
+  }
+}
